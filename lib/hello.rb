d17bce1b95424c7b9fba5604ff28baa3a7a756ba
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 20:11:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,13 +5,3 @@ def hello_t(array)
     i = i + 1 
   end
 end
-
-def pppp(name)
-  if name.start_with?("p") || name.start_with?("b")  then
-    puts "Hi, #{name}"
-  end
-end
-
-# call your method here!
-hello_t(["bob", "lois b", "pppp"]) do |name| pppp(name)
-end
