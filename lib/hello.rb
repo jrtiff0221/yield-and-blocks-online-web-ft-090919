4b96c7c8805666715ceb87a6cf4c479af02158bf
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 20:10:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(array)
+  i = 0 
+  while i < array.length
+    yield array[i]
+    i = i + 1 
+  end
+end
 
+def pppp(name)
+  if name.start_with?("p") || name.start_with?("b")  then
+    puts "Hi, #{name}"
+  end
 end
 
 # call your method here!
-
+hello_t(["bob", "lois b", "pppp"]) do |name| pppp(name)
+end
